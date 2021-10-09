d32d2c26a73452da28b2c29c2f3af4249c147480
ljbrewer <ljbrewer.lb@gmail.com>
2021-10-09 16:03:36 -0700

    updating

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'Set Null'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id',
+  onDelete: 'Set Null'
+});
 // Tags belongToMany Products (through ProductTag)
+
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id',
+  onDelete: 'Set Null'
+});
 
 module.exports = {
   Product,
